eb67b7fe3aeda6997cc7c089b53e944661ab0636
MaiAnT1 <135111560+MaiAnT1@users.noreply.github.com>
2023-06-03 08:56:23 +0700

    Add files via upload

---
+++ example.js
@@ -0,0 +1,57 @@
+function fetchData() {
+    fetch('https://reqres.in/api/users?page=2')
+        .then(response => {
+            if (!response.ok) {
+                throw Error("ERROR")
+            }
+            return response.json();
+        })
+        .then(data => {
+            const html = data.data
+                .map(user => {
+                  return `
+                   <div class="user">
+                      <p><img src="${user.avatar}" alt="${user.first_name}" /></p>
+                      <p>Name: ${user.first_name}</p>
+                      <p>Email: ${user.email}</p>
+                    </div>
+                  `;
+                })
+                .join("")
+            document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+        })
+        .catch(error =>{
+            console.log(error);
+        });
+    
+}
+
+fetchData();
+
+function postData() {
+    fetch('https://reqres.in/api/users?page=2', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+                name: "morpheus",
+                job: "leader"
+            })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw Error("ERROR")
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error =>{
+            console.log(error);
+        });
+    
+}
+
+postData();
